db24b9b04a2a7ef190353d84ac04585bbafced1c
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-05-02 12:11:05 +0200

    Add person management to Main and PersonRepository classes
    This commit updates the Main and PersonRepository classes by introducing several new person instances and adding them to a newly created PersonRepository. It also includes a mechanism to get a person by ID, with appropriate handling for missing persons.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,30 @@
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println(DayOfWeek.MONDAY.checkWeekend());
         System.out.println(DayOfWeek.SATURDAY.checkWeekend());
+
+        Person aljoscha = new Person(1, "Aljoscha", DayOfWeek.MONDAY);
+        Person benjamin = new Person(2,"Benjamin",DayOfWeek.SATURDAY);
+        Person horst = new Person(3,"Horst",DayOfWeek.FRIDAY);
+        Person peter = new Person(4,"Peter",DayOfWeek.SUNDAY);
+
+        PersonRepository aljoschasContacts = new PersonRepository();
+        aljoschasContacts.addPerson(aljoscha);
+        aljoschasContacts.addPerson(horst);
+        aljoschasContacts.addPerson(peter);
+
+        System.out.println(aljoschasContacts);
+        System.out.println(aljoschasContacts.getPersonById(1));
+        System.out.println(aljoschasContacts.getPersonById(2));
+
+        Optional<Person> p = aljoschasContacts.getPersonById(4);
+        if (p.isPresent()) {
+            System.out.println(p.get().name() + "s favorite day of the week is " + p.get().favoriteDay());
+        } else {
+            System.out.println("Person does not exist");
+        }
     }
 }
--- src/main/java/PersonRepository.java
+++ src/main/java/PersonRepository.java
@@ -5,6 +5,17 @@ import java.util.Optional;
 public class PersonRepository {
     List<Person> persons = new ArrayList<>();
 
+    public void addPerson(Person person) {
+        persons.add(person);
+    }
+
+    @Override
+    public String toString() {
+        return "PersonRepository{" +
+                "persons=" + persons +
+                '}';
+    }
+
     public Optional<Person> getPersonById(int id) {
         for (Person person : persons) {
             if (person.id() == id) {
