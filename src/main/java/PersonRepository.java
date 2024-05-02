919d0ad7867211cbd0050ee565120bfc9bf88aaf
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-05-02 11:56:36 +0200

    Add method getPersonById in PersonRepository returning an Optional.

--- src/main/java/PersonRepository.java
+++ src/main/java/PersonRepository.java
@@ -1,6 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class PersonRepository {
     List<Person> persons = new ArrayList<>();
+
+    public Optional<Person> getPersonById(int id) {
+        for (Person person : persons) {
+            if (person.id() == id) {
+                return Optional.of(person);
+            }
+        }
+        return Optional.empty();
+    }
 }
